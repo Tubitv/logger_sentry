64a292d677f505a76eb4a0dc833ce7b89fbdd08b
taotao.lin <cnredink@gmail.com>
2018-03-02 14:25:17 +0800

    modified method to log event

--- lib/sentry.ex
+++ lib/sentry.ex
@@ -6,7 +6,6 @@ defmodule Logger.Backends.Sentry do
   dashboard. This module have a set of interface functions:
 
     * get/set the log level
-    * get the log format
     * get/set the log metadata
 
   ## Config
@@ -16,7 +15,6 @@ defmodule Logger.Backends.Sentry do
       config :logger,
         backends: [:console, Logger.Backends.File],
         sentry: [level: :error,
-                 format: "$message",
                  metadata: []
                 ]
 
@@ -27,7 +25,7 @@ defmodule Logger.Backends.Sentry do
 
   @level_list [:debug, :info, :warn, :error]
   @metadata_list [:application, :module, :function, :file, :line, :pid]
-  defstruct [format: nil, metadata: nil, level: nil, other_config: nil]
+  defstruct [metadata: nil, level: nil, other_config: nil]
 
   @doc """
   Get the backend log level.
@@ -43,12 +41,6 @@ defmodule Logger.Backends.Sentry do
     :gen_event.call(Logger, __MODULE__, {:level, level})
   end
   def level(_), do: :error_level
-
-  @doc """
-  Get the backend log format.
-  """
-  @spec format :: list()
-  def format, do: :gen_event.call(Logger, __MODULE__, :format)
 
   @doc """
   Get the backend log metadata.
@@ -88,10 +80,6 @@ defmodule Logger.Backends.Sentry do
     {:ok, :ok, %{state | level: level}}
   end
 
-  def handle_call(:format, state) do
-    {:ok, state.format, state}
-  end
-
   def handle_call(:metadata, state) do
     {:ok, state.metadata, state}
   end
@@ -105,11 +93,11 @@ defmodule Logger.Backends.Sentry do
     {:ok, state}
   end
 
-  def handle_event({level, _gl, {Logger, msg, ts, md}},
+  def handle_event({level, _gl, {Logger, msg, _ts, md}},
                    %{level: log_level} = state) do
     case meet_level?(level, log_level) do
       true ->
-        {:ok, log_event(level, md, format_event(level, msg, ts, md, state), state)}
+        {:ok, log_event(level, md, msg, state)}
       _ ->
         {:ok, state}
     end
@@ -137,24 +125,12 @@ defmodule Logger.Backends.Sentry do
   # private functions
   defp init(config, state) do
     level = Keyword.get(config, :level, :info)
-    format = Logger.Formatter.compile Keyword.get(config, :format)
     metadata = Keyword.get(config, :metadata, []) |> configure_metadata()
-    %{state | format: format, metadata: metadata, level: level}
+    %{state | metadata: metadata, level: level}
   end
 
   defp configure_metadata(:all), do: :all
   defp configure_metadata(metadata), do: Enum.reverse(metadata)
-
-  defp format_event(level, msg, ts, md, state) do
-    %{metadata: keys, format: format} = state
-    format
-    |> Logger.Formatter.format(level, msg, ts, take_metadata(md, keys))
-    |> :erlang.iolist_to_binary
-  end
-
-  defp take_metadata(_, []), do: []
-  defp take_metadata(metadata, :all), do: metadata
-  defp take_metadata(metadata, keys), do: Keyword.take(metadata, keys)
 
   defp meet_level?(_lvl, nil), do: true
   defp meet_level?(lvl, min) do
@@ -162,21 +138,23 @@ defmodule Logger.Backends.Sentry do
   end
 
 if Mix.env() in [:test] do
-  defp log_event(level, _metadata, output, state) do
+  defp log_event(level, _metadata, msg, state) do
     case :ets.info(:__just_prepare_for_logger_sentry__) do
       :undefined ->
         :ignore
       _ ->
-        :ets.insert(:__just_prepare_for_logger_sentry__, {level, output})
+        :ets.insert(:__just_prepare_for_logger_sentry__, {level, msg})
     end
     state
   end
 else
-  defp log_event(:error, metadata, output, state) do
+  defp log_event(:error, metadata, msg, state) do
+    {output, _} = Exception.blame(:error, msg, Keyword.get(metadata, :stacktrace, []))
     Sentry.capture_exception(output, metadata)
     state
   end
-  defp log_event(_level, metadata, output, state) do
+  defp log_event(level, metadata, msg, state) do
+    {output, _} = Exception.blame(level, msg, Keyword.get(metadata, :stacktrace, []))
     Sentry.capture_message(output, metadata)
     state
   end
