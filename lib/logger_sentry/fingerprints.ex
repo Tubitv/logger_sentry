225bee2d63b3d3feb54e4ec65f8145fb2e44351f
taotao.lin <cnredink@gmail.com>
2019-05-21 21:51:56 +0800

    changed the approach for get default fingerprints

--- lib/logger_sentry/fingerprints.ex
+++ lib/logger_sentry/fingerprints.ex
@@ -9,7 +9,52 @@ defmodule LoggerSentry.Fingerprints do
   def fingerprints(_metadata, msg) do
     msg
     |> :erlang.iolist_to_binary()
-    |> String.split("\n")
-    |> Enum.filter(fn i -> String.starts_with?(i, "** (") end)
+    |> String.split(["\n", "  "], trim: true)
+    |> Enum.map(&convert_error/1)
+    |> Enum.uniq()
+    |> Enum.reject(&is_nil/1)
   end
+
+  @doc false
+  defp convert_error("** (FunctionClauseError)" <> _) do
+    "(FunctionClauseError) no function clause matching"
+  end
+
+  defp convert_error("** (MatchError)" <> _) do
+    "(MatchError) no match of right hand side value"
+  end
+
+  defp convert_error("** (UndefinedFunctionError)" <> _) do
+    "UndefinedFunctionError"
+  end
+
+  defp convert_error("** (KeyError) key" <> _) do
+    "KeyError"
+  end
+
+  defp convert_error("** (EXIT) time out" <> _) do
+    "(EXIT) time out"
+  end
+
+  # db connection
+  defp convert_error("** (exit) %DBConnection.ConnectionError" <> _) do
+    "(exit) DBConnection ConnectionError"
+  end
+
+  defp convert_error("** (DBConnection.ConnectionError)" <> _) do
+    "(exit) DBConnection ConnectionError"
+  end
+
+  # fallback
+  defp convert_error(error) do
+    cond do
+      String.contains?(error, "** (DBConnection.ConnectionError)") ->
+        "(exit) DBConnection ConnectionError"
+
+      true ->
+        nil
+    end
+  end
+
+  # __end_of_module__
 end
